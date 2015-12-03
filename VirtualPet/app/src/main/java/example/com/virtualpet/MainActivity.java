faaee3dbb92ae78752e58aea36e906fdd83d3888
boschma2702 <reneboschma@hotmail.com>
2015-12-03 15:14:03 +0100

    Recourcemanager added

--- VirtualPet/app/src/main/java/example/com/virtualpet/MainActivity.java
+++ VirtualPet/app/src/main/java/example/com/virtualpet/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        new ResourceManager(this);
     }
 
 
---
+++ VirtualPet/app/src/main/java/example/com/virtualpet/ResourceManager.java
@@ -0,0 +1,40 @@
+package example.com.virtualpet;
+
+import android.app.Activity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
+
+/**
+ * Created by reneb_000 on 3-12-2015.
+ */
+public final class ResourceManager {
+
+    public static ResourceManager INSTANCE;
+
+    private int screenWidth, screenHeight;
+    private DisplayMetrics metrics;
+
+    public ResourceManager(Activity a){
+        DisplayMetrics metrics = new DisplayMetrics();
+        a.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        this.metrics = metrics;
+        screenWidth = metrics.widthPixels;
+        screenHeight = metrics.heightPixels;
+        log();
+        INSTANCE = this;
+    }
+
+    public float convertDpToPixel(float dp){
+        return dp * (metrics.densityDpi / 160f);
+    }
+
+    public float convertPixelsToDp(float px){
+        return px / (metrics.densityDpi / 160f);
+    }
+
+    private void log(){
+        String s = "Recoursemanager initialized. \nscreenwidth: "+screenWidth + "\nscreenheight: "+screenHeight;
+        Log.d("RecourceManager", s);
+    }
+}
