983b1f725d94770c6c6447d872d62843dd61b879
GijsBeernink <g.j.beernink@student.utwente.nl>
2015-12-14 20:12:02 +0100

    Dog AI

--- VirtualPet/app/src/main/java/example/com/virtualpet/Dog.java
+++ VirtualPet/app/src/main/java/example/com/virtualpet/Dog.java
@@ -2,7 +2,6 @@ package example.com.virtualpet;
 
 import android.content.Context;
 import android.util.Log;
-
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.TimeZone;
@@ -17,15 +16,19 @@ public class Dog {
     private DogView view;
     private int x, y, screenwidth, speed;
 
-    private int satisfaction = 0;
+    private int satisfaction;
     private static final int MAXSATISFACTION = 100;
     private static final int MINSATISFACTION = 0;
+    public static final long UPDATEINTERVAL = 60000; // check for updates every minute
+    public static final long THIRTYMINUTES = 1800000; // thirty minutes in milliseconds
+    private long lastUpdate;
 
     protected ArrayList<Calendar> eatTimes = new ArrayList<Calendar>();
     protected ArrayList<Calendar> walkTimes = new ArrayList<Calendar>();
     private Calendar now = Calendar.getInstance();
-    private boolean eaten;
-    private boolean walked;
+    private long lastEaten;
+    private long lastPlayed;
+    private boolean dirty;
 
 
     public Dog(Context c, DogView view){
@@ -46,20 +49,46 @@ public class Dog {
         x += speed;
         view.setXY(x, y);
 
-        checkUpdates();
+//      Update every given interval
+        if (getLastUpdate() - getTime() > UPDATEINTERVAL) {
+            checkUpdates();
+            setLastUpdate(getTime());
+        }
     }
 
     public void checkUpdates() {
         checkForEatTime();
         checkForWalkTime();
+        checkActivities();
     }
 
     public void initialize() {
         Log.e("dog", "Initializing...");
-        updateSatisfaction(50);
+        updateSatisfaction(60);
         now.setTimeZone(TimeZone.getTimeZone("Europe/Amsterdam"));
         setEatTimes();
         setWalkTimes();
+    }
+
+    public void checkActivities() {
+        if ((getTime() - getTimeLastPlayed()) > 6*THIRTYMINUTES) {
+//            TODO: Show animation bored
+        }
+        if (getDirty()) {
+//            TODO: Show animation dirty
+        }
+    }
+
+    public void playWithDog(boolean gettingDirty) {
+        if((getTime() - getTimeLastEaten()) > 2*THIRTYMINUTES) {
+            updateSatisfaction(15);
+            if(Math.random() < 0.75 && gettingDirty) {
+                setDirty(true);
+            }
+        } else {
+            // If an hour has not yet passed
+            updateSatisfaction(-10);
+        }
     }
 
     // Eating
@@ -77,9 +106,12 @@ public class Dog {
 
     public boolean checkForEatTime() {
         boolean eat = false;
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(getTime());
         for (int i = 0; i < eatTimes.size(); i++) {
-            if (eatTimes.get(i).get(Calendar.HOUR_OF_DAY) == getHourOfDay()) {
+            if (eatTimes.get(i).get(Calendar.HOUR_OF_DAY) == cal.get(Calendar.HOUR_OF_DAY)) {
                 eat = true;
+                // TODO: Show animation
             }
         }
         return eat;
@@ -103,14 +135,19 @@ public class Dog {
 
     public boolean checkForWalkTime() {
         boolean walk = false;
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(getTime());
         for (int i = 0; i < walkTimes.size(); i++) {
-            if (walkTimes.get(i).get(Calendar.HOUR_OF_DAY) == getHourOfDay()) {
+            if (walkTimes.get(i).get(Calendar.HOUR_OF_DAY) == cal.get(Calendar.HOUR_OF_DAY)) {
                 walk = true;
+                // TODO: Show animation
             }
         }
+
         return walk;
     }
 
+    // Satisfaction
     public void updateSatisfaction(int satisfaction) {
         if (this.satisfaction + satisfaction > MAXSATISFACTION) {
             this.satisfaction = MAXSATISFACTION;
@@ -126,8 +163,33 @@ public class Dog {
         return satisfaction;
     }
 
-    public int getHourOfDay(){
-        return now.get(Calendar.HOUR_OF_DAY);
+    // Getters & Setters
+    public long getTimeLastPlayed() {
+        return lastPlayed;
+    }
+
+    public long getTimeLastEaten() {
+        return lastEaten;
+    }
+
+    public boolean getDirty() {
+        return dirty;
+    }
+
+    public void setDirty(boolean dirty) {
+        this.dirty = dirty;
+    }
+
+    public long getTime(){
+        return now.getTimeInMillis();
+    }
+
+    public void setLastUpdate(long time) {
+        lastUpdate = time;
+    }
+
+    public long getLastUpdate() {
+        return lastUpdate;
     }
 
     public DogView getView() {
