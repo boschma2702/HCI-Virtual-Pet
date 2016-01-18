4b9610a8480d7bc34cc793ead95838a6eed900f7
Lex Favrin <a.s.favrin@student.utwente.nl>
2016-01-18 15:27:40 +0100

    Added significance to randomBark()

--- VirtualPet/app/src/main/java/example/com/virtualpet/Dog.java
+++ VirtualPet/app/src/main/java/example/com/virtualpet/Dog.java
@@ -60,7 +60,7 @@ public class Dog {
     }
 
     public void randomBark() {
-        if (Math.random() >= 0.99999) {
+        if (Math.random() >= 0.99) {
             DogService.INSTANCE.setBarking(true);
         }
     }
